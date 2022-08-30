a7092bef644ae71c950d236449a6441cf34173fb
afisman <afisman@gmail.com>
2022-08-30 18:12:16 +0200

    solved some more random questions

--- index.js
+++ index.js
@@ -150,6 +150,21 @@ const getPrivateVariable = secretVariable()
 
 console.log(getPrivateVariable())
 
+//What is the output
+
+var num = 4
+function outer() {
+    var num = 2
+    function inner() {
+        num++
+        var num = 3
+        console.log(num)
+    }
+    inner()
+}
+
+outer()
+
 
 
 
