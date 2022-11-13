242dc1ef703853dd8d972c8b4aab716cac6c18ad
afisman <afisman@gmail.com>
2022-11-13 23:50:01 +0100

    Fib memoization function

--- index.js
+++ index.js
@@ -226,6 +226,16 @@ fullData.forEach(eachPassword => {
 })
 
 
+//Return nth number of Fib sequence.
+
+const fib = (n, memo = {}) => {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
+
+};
 
 
 
