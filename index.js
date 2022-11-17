ab779978eaca8b238c3335bc8f8ac70a92db0f59
afisman <afisman@gmail.com>
2022-11-17 16:34:37 +0100

    canConstruct memoization

--- index.js
+++ index.js
@@ -313,3 +313,22 @@ const bestSum = (targetSum, numbers, memo = {}) => {
     memo[targetSum] = shortestCombination
     return shortestCombination;
 }
+
+//canConstruct memoization
+
+const canConstruct = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target];
+    if (target === '') return true;
+
+    for (let word of wordBank) {
+        if (target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            if (canConstruct(suffix, wordBank, memo) === true) {
+                memo[target] = true;
+                return true;
+            }
+        }
+    }
+    memo[target] = false;
+    return false;
+}
