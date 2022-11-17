e8322754e17fe2c56958cf83a6cca7bed4a1ca9a
afisman <afisman@gmail.com>
2022-11-17 15:00:03 +0100

    bestSum memoization

--- index.js
+++ index.js
@@ -289,3 +289,27 @@ const howSum = (targetSum, numbers, memo = {}) => {
     memo[targetSum] = null
     return null;
 }
+
+
+//bestSum memoization
+
+const bestSum = (targetSum, numbers, memo = {}) => {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+
+    let shortestCombination = null;
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        const remainderComb = bestSum(remainder, numbers, memo);
+        if (remainderComb !== null) {
+            const combination = [...remainderComb, num];
+            if (shortestCombination === null || combination.length < shortestCombination.length) {
+                shortestCombination = combination;
+            }
+
+        }
+    }
+    memo[targetSum] = shortestCombination
+    return shortestCombination;
+}
