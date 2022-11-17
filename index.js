6625b34b46207a2e36bb2a4b27cc248756807c4a
afisman <afisman@gmail.com>
2022-11-17 12:35:10 +0100

    howSum memoization

--- index.js
+++ index.js
@@ -269,3 +269,23 @@ const canSum = (targetSum, numbers, memo = {}) => {
 
     return false;
 }
+
+//howSum meoization
+
+const howSum = (targetSum, numbers, memo = {}) => {
+    if (targetSum in memo) return memo[targetSum]
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        const remainderResult = howSum(remainder, numbers, memo);
+        if (remainderResult !== null) {
+            memo[targetSum] = [...remainderResult, num];
+            return memo[targetSum];
+        }
+    }
+
+    memo[targetSum] = null
+    return null;
+}
